bed724eebbd764636d51035f84287fdf5e6c9fe5
root <root@SURFACE.>
2024-03-20 09:10:28 +0300

    declaration

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName= "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomerName(){
+   bestCustomer = " not bob";
+}
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFAvouriteCustomer="you";
+function changeLeastFavouriteCustomer(){
+    leastFAvouriteCustomer = "tony"
+}
